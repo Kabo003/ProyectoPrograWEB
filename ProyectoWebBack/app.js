067348babe8ea53b842b3b949d6ac636b3ff0fc8
cyvo1912 <20212853@aloe.ulima.edu.pe>
2024-11-30 18:07:02 -0500

    cambio archivo

--- ProyectoWebBack/app.js
+++ ProyectoWebBack/app.js
@@ -29,31 +29,6 @@ async function verificarConexion(){
     }
 }
 
-const crearUsuarios = async () => {
-    try {
-        await Usuario.create({
-            nombre: 'Piero Gutierrez',
-            contraseña: 'Guty123', // Asegúrate de encriptar las contraseñas en un entorno real
-            correo: 'pierog@gmail.com',
-            telefono: '987654321',
-            admi: false
-        });
-
-        await Usuario.create({
-            nombre: 'Gabriel Tieno',
-            contraseña: 'Kabo742',
-            correo: 'Kabo742@gmail.com',
-            telefono: '912345678',
-            admi: true
-        });
-
-        console.log("Usuarios creados exitosamente.");
-    } catch (error) {
-        console.error("Error al crear los usuarios:", error);
-    }
-};
-
-crearUsuarios();
 
 app.listen(port, () => {
     console.log("Servidor esta activo en puerto " + port);
