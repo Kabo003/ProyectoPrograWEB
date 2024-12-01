cedbf241d2df99a975ec81185f783c1553160e7e
cyvo1912 <20212853@aloe.ulima.edu.pe>
2024-12-01 17:55:11 -0500

    cambio en front

--- ProyectoWebFront/src/modules/auth/services/adminServices.jsx
+++ ProyectoWebFront/src/modules/auth/services/adminServices.jsx
@@ -41,7 +41,7 @@ function adminServices() {
 
     return(
         <div>
-
+            
         </div>
     )
 }
--- ProyectoWebFront/src/navigation/AppNavigator.jsx
+++ ProyectoWebFront/src/navigation/AppNavigator.jsx
@@ -4,6 +4,7 @@ import HomeScreen from "../modules/auth/screen/HomeScreen";
 import LoginScreen from "../modules/auth/screen/LoginScreen";
 import BuscarScreen from "../modules/auth/screen/BuscarScreen";
 import DetalleProducto from "../modules/auth/screen/DetalleProducto";
+import { ProfileCard } from "../modules/profile/componentes/ProfileCard";
 
 
 
@@ -15,6 +16,7 @@ const AppNavigator=()=>{
         <Route path="/pe/es/shop/cart" element={<HomeScreen></HomeScreen>} />
         <Route path="/pe/es/search/home" element={<BuscarScreen />} />
         <Route path="/pe/es/product/"  element={<DetalleProducto/>}/>
+        <Route path="/pe/es/admin" element={<ProfileCard></ProfileCard>}></Route>
         
 
 
