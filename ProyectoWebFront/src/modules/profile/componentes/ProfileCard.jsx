29e3815c17403507ad7d6a0d24966a231672b722
cyvo1912 <20212853@aloe.ulima.edu.pe>
2024-12-01 19:47:36 -0500

    front

--- ProyectoWebFront/src/modules/profile/componentes/ProfileCard.jsx
+++ ProyectoWebFront/src/modules/profile/componentes/ProfileCard.jsx
@@ -1,7 +1,37 @@
-export const ProfileCard = () => {
-    return(
-        <div>
-            
-        </div>
-    )
-}
+/*
+"Producto", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nombre_producto: {
+            type: DataTypes.STRING
+        },
+        imagen: {
+            type: DataTypes.STRING
+        },
+        precio: {
+            type: DataTypes.DECIMAL(10, 2)
+        },
+        descripcion: {
+            type: DataTypes.STRING
+        },
+        color: {
+            type: DataTypes.STRING
+        },
+        stock: {
+            type: DataTypes.INTEGER
+        },
+        talla: {
+            type: DataTypes.STRING
+        },
+        categoria_id: {
+            type: DataTypes.INTEGER
+        }
+    }, {
+        freezeTableName: true
+    }
+);
+
+*/
